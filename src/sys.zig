eb72cdadf31cc0af8d04c319477fa156a266cdca
00JCIV00 <jake.crawfordiv@gmail.com>
2025-02-02 12:20:12 -0500

    Updated 'sys.zig'
    - Added the `getUID()` function to get the user that's running the process.
    - Fixed erroneous, program-stopping errors in `getPIDs()`.

--- src/sys.zig
+++ src/sys.zig
@@ -10,6 +10,7 @@ const mem = std.mem;
 const net = std.net;
 const os = std.os;
 const posix = std.posix;
+const time = std.time;
 
 
 /// Set the System's Hostname
@@ -31,6 +32,25 @@ pub fn setHostName(hostname: []const u8) !void {
         0 => {},
         else => return error.OSError,
     };
+}
+
+/// Get the System's Current User ID
+pub fn getUID() !posix.uid_t {
+    const get_uid = switch (builtin.target.os.tag) {
+        .linux => switch (builtin.target.cpu.arch) {
+            .arm => os.linux.syscalls.Arm.getuid,
+            .aarch64 => os.linux.syscalls.Arm64.getuid,
+            .x86 => os.linux.syscalls.X86.getuid,
+            .x86_64 => os.linux.syscalls.X64.getuid,
+            .mips, .mipsel => os.linux.syscalls.Mips.getuid,
+            .mips64, .mips64el => os.linux.syscalls.Mips64.getuid,
+            else => @compileError("Unsupported Architecture."),
+        },
+        else => @compileError("Unsupported OS."),
+    };
+    const uid = os.linux.syscall2(get_uid, 0, 0);
+    if (uid < 0) return error.OSError;
+    return @intCast(uid);
 }
 
 /// Get the System's Current User
@@ -64,15 +84,16 @@ pub fn getPIDs(alloc: mem.Allocator, proc_names: []const []const u8) ![]const u3
     var proc_dir = try fs.openDirAbsolute("/proc", .{ .iterate = true });
     defer proc_dir.close();
     var pids_list: std.ArrayListUnmanaged(u32) = .{};
+    errdefer pids_list.deinit(alloc);
     var proc_iter = proc_dir.iterate();
     while (try proc_iter.next()) |entry| {
         const pid = fmt.parseInt(u32, entry.name, 10) catch continue;
-        var pid_dir = try proc_dir.openDir(entry.name, .{});
+        var pid_dir = proc_dir.openDir(entry.name, .{}) catch continue;
         defer pid_dir.close();
-        var comm = try pid_dir.openFile("comm", .{});
+        var comm = pid_dir.openFile("comm", .{}) catch continue;
         defer comm.close();
         var pid_name_buf: [posix.PATH_MAX]u8 = .{ 0 } ** posix.PATH_MAX;
-        const pid_name_len = try comm.readAll(pid_name_buf[0..]);
+        const pid_name_len = comm.readAll(pid_name_buf[0..]) catch continue;
         for (proc_names) |proc_name| {
             if (mem.indexOf(u8, pid_name_buf[0..pid_name_len], proc_name) == null) continue;
             try pids_list.append(alloc, pid);
