d4991f0b0d570aecf01fb52f8352c5c9a8fcff8a
00jciv00 <jake.crawfordiv@gmail.com>
2025-06-02 04:13:39 -0400

    Updated build & repo files
    - Added a `.gitignore` f/ portability.
    - Removed libvaxis temporarily as the project is focused on the backend currenty.

--- build.zig
+++ build.zig
@@ -10,8 +10,8 @@ pub fn build(b: *std.Build) void {
     const cova_dep = b.dependency("cova", .{ .target = target, .optimize = optimize });
     const cova_mod = cova_dep.module("cova");
     // Vaxis
-    const vaxis_dep = b.dependency("vaxis", .{ .target = target, .optimize = optimize });
-    const vaxis_mod = vaxis_dep.module("vaxis");
+    //const vaxis_dep = b.dependency("vaxis", .{ .target = target, .optimize = optimize });
+    //const vaxis_mod = vaxis_dep.module("vaxis");
     // Zeit
     const zeit_dep = b.dependency("zeit", .{ .target = target, .optimize = optimize });
     const zeit_mod = zeit_dep.module("zeit");
@@ -32,10 +32,11 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+        //.use_llvm = false,
         //.sanitize_thread = if (optimize == .Debug) true else null,
     });
     exe.root_module.addImport("cova", cova_mod);
-    exe.root_module.addImport("vaxis", vaxis_mod);
+    //exe.root_module.addImport("vaxis", vaxis_mod);
     exe.root_module.addImport("zeit", zeit_mod);
     exe.root_module.addImport("oui_table", oui_lookup_mod);
     exe.root_module.addImport("config_fields", config_fields_mod);
