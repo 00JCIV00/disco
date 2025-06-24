f32044b79aa69a75e23fb29dd58ebcdd8b409e03
00jciv00 <jake.crawfordiv@gmail.com>
2025-06-24 18:33:09 -0400

    Added Library Modules
    - Added all self-containable (or mostly so) libraries as independent modules in `build.zig`.

--- build.zig
+++ build.zig
@@ -42,15 +42,21 @@ pub fn build(b: *std.Build) void {
     exe.root_module.addImport("config_fields", config_fields_mod);
     b.installArtifact(exe);
 
+    // Exe Testing
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
         .strip = optimize != .Debug,
     });
-
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    // Library Modules
+    _ = b.addModule("dbus", .{ .root_source_file = b.path("src/dbus.zig") });
+    _ = b.addModule("netdata", .{ .root_source_file = b.path("src/netdata.zig") });
+    _ = b.addModule("netlink", .{ .root_source_file = b.path("src/netlink.zig") });
+    _ = b.addModule("netprotocols", .{ .root_source_file = b.path("src/protocols.zig") });
+    _ = b.addModule("sys", .{ .root_source_file = b.path("src/sys.zig") });
 }
